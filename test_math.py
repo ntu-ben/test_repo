1eb17d4b2b77f786689ce6bd147c0c446ff5406d
BEN_HO <pohan.ho@gmail.com>
2023-04-14 15:52:58 +0800

    [dev] bug fix

--- test_math.py
+++ test_math.py
@@ -1,23 +1,19 @@
-# import math.py
+import unittest
 from math import add, subtract, multiply, divide
 
-def test_add():
-    assert add(1, 2) == 3
+class TestMath(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
 
-def test_subtract():
-    assert subtract(1, 2) == -1
+    def test_subtract(self):
+        self.assertEqual(subtract(1, 2), -1)
 
-def test_multiply():
-    assert multiply(1, 2) == 2
+    def test_multiply(self):
+        self.assertEqual(multiply(1, 2), 2)
 
-def test_divide():
-    assert divide(1, 2) == 0.5
-
-def main():
-    test_add()
-    test_subtract()
-    test_multiply()
-    test_divide()
+    def test_divide(self):
+        self.assertEqual(divide(1, 2), 0.5)
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
+
